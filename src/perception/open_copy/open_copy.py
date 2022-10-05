66a90cbe9694e8654a1bc937ab647d2a4031be2f
GXR1LL4Z <crisza2001@gmail.com>
2022-10-05 15:55:26 +0200

    zatwierdz

--- src/perception/open_copy/open_copy.py
+++ src/perception/open_copy/open_copy.py
@@ -5,16 +5,13 @@ import cv2
 image_name = "knr"
 
 print("Odczyt obrazu z pliku")
-img = cv2.imread("open_copy/"+image_name+"/.jpg")
+img = cv2.imread("/home/krzysztof/catkin_ws/src/hello_world/src/perception/open_copy/"+image_name+".jpg")
 
 print("Stworz miejsce dla obrazu")
-cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+cv2.namedWindow("Image", cv2.WINDOW_AUTOSIZE)
 
 print("Wyswietl obraz")
 cv2.imshow("Image", img)
 
 print("Nacisnij klawisz 0 aby zrobic kopie")
-cv2.waitKet(0)
-
-print("Obraz skopiowano do folderu copy")
-cv2.imwrite("copy/"+image_name+"/-copy.jpg", img)
+cv2.waitKey(0)
