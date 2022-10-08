dff24c85722479274ba6a314ba6f0682f60a85af
GXR1LL4Z <crisza2001@gmail.com>
2022-10-08 22:52:55 +0200

    Create cv.py

---
+++ src/perception/CVBridge/CVBridge.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import sys
+
+
+#uruchamianie kamery w rosie
+#$rosrun usb_cam usb_cam_node _pixel_format:=mjpeg
+
+
+#sworzyc obiekt typu CvBridge
+bridge = CvBridge()
+
+def image_callback(ros_image):
+    global bridge 
+
+    #konwersja z typu rosa na typ opencv
+    try: 
+        cv_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+    #od teraz pracujemy z obrazem tak jak zawsze w opencv
+    cv2.imshow("Image Window", cv_image)
+    cv2.waitKey(3)
+
+
+def main(argv):
+    rospy.init_node('image_converter', anonymous = True)
+    image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, image_callback)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("THe proces has been shot down")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+    #..
+    #
+    #
+    #..
--- src/perception/camera/camera.py
+++ src/perception/camera/camera.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 import numpy as np
 import cv2 
-captured_video = cv2.VideoCapture(0)
+#odczyt filmu ze sciezki
+captured_video = cv2.VideoCapture('/home/krzysztof/catkin_ws/src/hello_world/src/perception/camera/kule.mov')
+#odczyt streama z kamery na zywo
+#captured_video = cv2.VideoCapture(0)
 if not captured_video.isOpened():
     print("Cannot open camera")
     exit()
@@ -9,13 +12,16 @@ while True:
     # Capture frame-by-frame
     ret, frame = captured_video.read()
     # if frame is read correctly ret is True
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #zmiana ksztatu 
+    frame = cv2.resize(frame, (0,0), fx = 0.5, fy = 0.5)
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     
     # Display the resulting frame
     cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(25) == ord('q'):
         break
 # When everything done, release the capture
 captured_video.release()
