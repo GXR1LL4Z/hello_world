7ea8eda7ba46eb87b4f9f5359a741e8d83a65f69
GXR1LL4Z <crisza2001@gmail.com>
2022-10-08 19:58:45 +0200

    zatwierdz

--- src/scripts/IoT_sensor/IoT_sensor_sub.py
+++ src/scripts/IoT_sensor/IoT_sensor_sub.py
@@ -5,7 +5,7 @@ from hello_world.msg import IoTsensor
 def iot_sensor_callback(iot_sensor_message):
     rospy.loginfo("new IoT data received: (%d, %s, %.2f ,%.2f)",iot_sensor_message.id,iot_sensor_message.name,
     iot_sensor_message.temperature,iot_sensor_message.humidity)
-    
+
 rospy.init_node('iot_sensor_subscriber_node', anonymous=True)
 
 rospy.Subscriber("iot_sensor_topic", IoTsensor, iot_sensor_callback)
