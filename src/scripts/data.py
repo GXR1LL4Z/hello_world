67fb4bfdf9426226e9797edca7a575ac37d2c206
GXR1LL4Z <crisza2001@gmail.com>
2022-10-15 23:31:01 +0200

    tb2

--- src/scripts/data.py
+++ src/scripts/data.py
@@ -2,6 +2,7 @@
 import serial.tools.list_ports
 import rospy
 from geometry_msgs.msg import Vector3
+import struct
 
 
 ports = serial.tools.list_ports.comports()
@@ -19,19 +20,32 @@ serialInst.open()
 data_test = Vector3()
 while True:
     if serialInst.in_waiting:
-        line = serialInst.readline()
-        line.decode('utf')
+        line = str(serialInst.readline())
         
-        if line.startswith(b"X"):
-            data_test.x = line
-            #print(data_test.x)
-        if line.startswith(b"Y"):
-            data_test.y = line
-            #print(data_test.y)
-        if line.startswith(b"Z"):
-            data_test.z = line
-            #print(data_test.z)
-        print(data_test)
+        if line[2] == "X":
+            print("To jest X")
+            if serialInst.in_waiting:
+                line = str(serialInst.readline())
+                var = float(line[2:-5])
+                print(var)
+                print(type(var))
+        if line[2] == "Y":
+            print("To jest Y")
+            if serialInst.in_waiting:
+                line = str(serialInst.readline())
+                var = float(line[2:-5])
+                print(var)
+                print(type(var))
+        if line[2] == "Z":
+            print("To jest Z")
+            if serialInst.in_waiting:
+                line = str(serialInst.readline())
+                var = float(line[2:-5])
+                print(var)
+                print(type(var))
 
 #jesli odmoa dostepu do portu:
 #sudo chmod 666 /dev/ttyACM0  
+#dziwne to rozwiazanie 
+#sprawdzic jak działa bez delaya
+#jak wysyłac zmienne float przez uart
