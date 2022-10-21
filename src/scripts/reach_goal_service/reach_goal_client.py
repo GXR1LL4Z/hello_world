8774df9ba48fc99d49cc839b24290354b35c900d
GXR1LL4Z <crisza2001@gmail.com>
2022-10-21 19:50:58 +0200

    tb2

--- src/scripts/reach_goal_service/reach_goal_client.py
+++ src/scripts/reach_goal_service/reach_goal_client.py
@@ -3,3 +3,38 @@ from hello_world.srv import reach_goalRequest
 from hello_world.srv import reach_goalResponse
 import rospy
 import sys
+
+class reach_goal_client:    
+    def __init__(self):
+        self.request()
+
+    def client(self, x_goal, y_goal, z_goal, x, y, z):
+        rospy.wait_for_service('reach_goal')
+        try: 
+            reach_goals = rospy.ServiceProxy('reach_goal', reach_goal)
+            response = reach_goals(x_goal, y_goal, z_goal, x, y, z)
+            return response.result
+        except rospy.ServiceException:
+            pass
+
+    def request(self):
+        if len(sys.argv) == 7:
+            x_goal = int(sys.argv[1])
+            y_goal = int(sys.argv[2])
+            z_goal = int(sys.argv[3])
+            x = int(sys.argv[4])
+            y = int(sys.argv[5])
+            z = int(sys.argv[6])
+            print(x_goal)
+            print(y_goal)
+            print(z_goal)
+            print(x)
+            print(y)
+            print(z)
+        else:
+            sys.exit(1)
+        result = self.client(x_goal, y_goal, z_goal, x, y, z)
+        print("{}".format(result))
+
+if __name__ == '__main__':
+    reach_goal_client()
--- src/scripts/reach_goal_service/reach_goal_server.py
+++ src/scripts/reach_goal_service/reach_goal_server.py
@@ -2,3 +2,42 @@ from hello_world.srv import reach_goal
 from hello_world.srv import reach_goalRequest
 from hello_world.srv import reach_goalResponse
 import rospy
+
+
+
+class reach_goal_server:
+    def __init__(self):
+        self.s = rospy.Service('reach_goal', reach_goal, self.handle_reach_goal)
+        print("gotowy")
+        rospy.spin()
+
+    def handle_reach_goal(self, request):
+        dx = request.x_goal - request.x
+        dy = request.y_goal - request.y
+        dz = request.z_goal - request.z
+        # print(request.x_goal)
+        # print(request.y_goal)
+        # print(request.z_goal)
+        # print(request.x)
+        # print(request.y)
+        # print(request.z)
+        # print(dx)
+        # print(dy)
+        # print(dz)
+        while True:
+            dx = request.x_goal - request.x
+            dy = request.y_goal - request.y
+            dz = request.z_goal - request.z
+            if dx == 0 and dy == 0 and dz ==0:
+                return reach_goalResponse(True)
+            else:
+                #return reach_goalResponse(False)
+                request.x = request.x + dx
+                request.y = request.y + dy
+                request.z = request.z + dz
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('reach_goal_server', anonymous = True)
+    reach_goal_server() 
