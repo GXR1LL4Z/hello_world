9f82236d9f408f36b80bda0b88306fa0aa0b44be
GXR1LL4Z <crisza2001@gmail.com>
2022-10-30 19:27:03 +0100

    tb2

--- src/scripts/power_service/power_client.py
+++ src/scripts/power_service/power_client.py
@@ -1,8 +1,21 @@
+from ast import Pass
 import rospy
+
+from hello_world.srv import power_service
+from hello_world.srv import power_serviceRequest
 
 class power_client:
     def __init__(self):
-        pass
+        #stworzyc clienta
+        rospy.wait_for_service('power_service')
+        self.client = rospy.ServiceProxy('power_service', power_service)
+        request = power_serviceRequest(number = 2, power = 3)
+        try:
+            #zwrocenie wyniku
+            response = self.client(request)
+            rospy.loginfo("RESPONSE: "+ str(response))
+        except rospy.ServiceException():
+            pass
 
 if __name__ == '__main__':
     rospy.init_node('power_client', anonymous = True)
--- src/scripts/power_service/power_server.py
+++ src/scripts/power_service/power_server.py
@@ -1,8 +1,17 @@
 import rospy
+from hello_world.srv import power_service
+from hello_world.srv import power_serviceResponse
 
 class power_server:
     def __init__(self):
-        pass
+        self.server = rospy.Service('power_service', power_service, self.handle_response)
+        rospy.loginfo("SERVER HAS BEEN STARTED")
+
+    def handle_response(self, request):
+        rospy.loginfo("SERVER HAS RECEIVED REQUEST FROM CLIENT")
+        response = power_serviceResponse()
+        response.result = request.number**request.power
+        return response
 
 if __name__ == '__main__':
     rospy.init_node('power_server', anonymous = True)
